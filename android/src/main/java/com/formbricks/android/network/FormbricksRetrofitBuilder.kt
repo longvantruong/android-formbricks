d74caee731a462254480b52f4474587ac137a993
longvantruong <longtv.mobiledeveloper@gmail.com>
2025-05-28 11:09:03 +0700

    test trust ssl

--- android/src/main/java/com/formbricks/android/network/FormbricksRetrofitBuilder.kt
+++ android/src/main/java/com/formbricks/android/network/FormbricksRetrofitBuilder.kt
@@ -4,12 +4,46 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class FormbricksRetrofitBuilder(private val baseUrl: String, private val loggingEnabled: Boolean) {
 
+    private fun getTrustManager(): Array<TrustManager> {
+        return arrayOf(
+            object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+                }
+
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+                }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            }
+        )
+    }
+
+    private fun getSSLSocketFactory(): SSLSocketFactory {
+        return try {
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, getTrustManager(), SecureRandom())
+            sslContext.socketFactory
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
     fun getBuilder(): Retrofit.Builder {
         val clientBuilder = OkHttpClient.Builder()
+            .sslSocketFactory(getSSLSocketFactory(), getTrustManager()[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
             .connectTimeout(CONNECT_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS)
             .readTimeout(READ_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS)
             .followSslRedirects(true)
